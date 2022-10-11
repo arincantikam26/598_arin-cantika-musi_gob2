a4de84c8367b57be578ffba79679f8bb1619b962
arincantikam26 <arincantikam@gmail.com>
2022-10-12 06:41:57 +0700

    added sesi-08

---
+++ sesi-08/decoding_parsing/map_decode.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Employee struct {
+	FullName string `json:"full_ame"`
+	Email    string `json:"email"`
+	Age      int    `json:"age"`
+}
+
+func main() {
+	var jsonString = `
+	{
+		"full_name":"Airell Jordan",
+		"email":"airell@mail.com",
+		"age":23
+	}
+	`
+
+	var result map[string]interface{}
+
+	var err = json.Unmarshal([]byte(jsonString), &result)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("full_name:", result["full_name"])
+	fmt.Println("email:", result["email"])
+	fmt.Println("age: ", result["age"])
+
+}
---
+++ sesi-08/readme-sesi08.md
@@ -0,0 +1,6 @@
+# SESI 08
+- kode peserta    : 149368582100-598
+- nama lengkap    : Arin Cantika Musi
+
+---
+## Summary sesi 8 : 
